ccc96b23493e5122d3babc5dd35f5318074ee5dd
Thibault Chipy <tchipy@iutbayonne.univ-pau.fr>
2024-11-12 11:37:41 +0100

    Update constantes.php

--- config/constantes.php
+++ config/constantes.php
@@ -6,7 +6,7 @@ define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
 define('DB_NAME', 'namrein_pro');
 define('DB_USER', 'namrein_pro');
 define('DB_PASS', 'namrein_pro');
-define('PREFIXE_TABLE', '');
+define('PREFIXE_TABLE', 'vhs');
 
 // Constante de la partie vue
 define('WEBSITE_TITLE', 'VHS - Video Home Share');
