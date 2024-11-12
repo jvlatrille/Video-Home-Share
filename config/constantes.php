475f501a377a910da7b978c1ac53ccbdabe6a4e5
Lea Despre-Hildevert <l.despre-hildevert@iutbayonne.univ-pau.fr>
2024-11-12 11:40:32 +0100

    changement constantes en namrein

--- config/constantes.php
+++ config/constantes.php
@@ -3,9 +3,9 @@
 // Ce ne sont que des exemples
 //Connexion à la base de donnée
 define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
-define('DB_NAME', 'abaros_pro');
-define('DB_USER', 'abaros_pro');
-define('DB_PASS', 'abaros_pro');
+define('DB_NAME', 'namrein_pro');
+define('DB_USER', 'namrein_pro');
+define('DB_PASS', 'namrein_pro');
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'vhs');
