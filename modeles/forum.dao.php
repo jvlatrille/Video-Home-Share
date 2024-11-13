90a7023f27efca92c487d6038bfb23eb3a99c585
Aymeric Pigeon <apigeon001@iutbayonne.univ-pau.fr>
2024-11-13 16:03:35 +0100

    Création du fichier modele forum.dao

---
+++ modeles/forum.dao.php
@@ -0,0 +1,69 @@
+<?php 
+
+class forumDAO{
+    private ?PDO $pdo;
+
+    public function __construct(PDO $pdo){
+        $this->pdo = $pdo;
+    }
+
+    //Getters et setters
+    public function getPdo(): ?PDO{
+        return $this->pdo;
+    }
+
+    public function setPdo(?PDO $pdo): void{
+        $this->pdo = $pdo;
+    }
+
+    //Méthode pour récupérer un forum
+    public function find(?int $idForum): ?Forum {
+        $sql = "SELECT * FROM ".PREFIXE_TABLE."forum WHERE idForum = :id";
+        $pdoStatement = $this->pdo->prepare($sql);
+        $pdoStatement->execute(array('idForum' => $idForum));
+        $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+        $forumData = $pdoStatement->fetch();   
+        return $forumData ? $this->hydrate($forumData) : null;
+    }
+    
+
+    //Méthode pour récupérer tout les forums
+    public function findAll() {
+        
+        $sql = "SELECT * FROM " . PREFIXE_TABLE . "oa";
+        $pdoStatement = $this->pdo->prepare($sql);
+        $pdoStatement->execute();
+        $pdoStatement->setFetchMode(PDO::FETCH_ASSOC);  
+
+        $resultats = $pdoStatement->fetchAll();
+
+        // Appelle hydrateAll pour transformer tous les enregistrements en objets OA
+        return $this->hydrateAll($resultats);
+    }
+    public function hydrate($tableauAssoc) : ?Forum{
+        $forum=new Forum();
+        $forum->setId($tableauAssoc['idForum']);
+        $forum->setNom($tableauAssoc['nom']);
+        $forum->setNote($tableauAssoc['description']);
+        $forum->setType($tableauAssoc['theme']);
+        return $forum;
+    }
+
+    public function hydrateAll(array $resultats): ?array {
+        $forumListe = [];
+        foreach ($resultats as $row) {
+            $forumListe[] = $this->hydrate($row);
+        }
+        
+        return $forumListe;
+    }
+    
+
+
+
+
+
+
+
+}
+?>
