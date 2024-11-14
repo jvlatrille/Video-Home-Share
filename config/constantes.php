f486eb784ecc29c5f9a5bd8147facc0602240d1c
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-11-14 17:15:45 +0100

    Retrait fichier image innutile + correction des constantes de la BD

--- config/constantes.php
+++ config/constantes.php
@@ -2,12 +2,11 @@
 
 // Ce ne sont que des exemples
 //Connexion à la base de donnée
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'vhs');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
-define('PREFIXE_TABLE', 'vhs_');
+define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
+define('DB_NAME', 'namrein_pro');
+define('DB_USER', 'namrein_pro');
+define('DB_PASS', 'namrein_pro');
+define('PREFIXE_TABLE', 'vhs');
 
 // Constante de la partie vue
 define('WEBSITE_TITLE', 'VHS - Video Home Share');
