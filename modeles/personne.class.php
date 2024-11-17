24500019cd1fab8f9c0f1ec75407b7638a66c35c
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-11-17 12:34:01 +0100

    Documentation de personne + Quelques corrections

--- modeles/personne.class.php
+++ modeles/personne.class.php
@@ -1,15 +1,53 @@
 <?php
 
+/**
+ * @file personne.class.php
+ * @author VINET LATRILLE Jules
+ * @brief Classe Personne pour représenter une personne
+ * @details Cette classe permet de représenter une personne avec ses attributs (idPersonne, nom, prénom, dateNaiss, genre).
+ * @version 1.0
+ * @date : 17/11/2024
+ */
 class Personne
 {
-    // Attributs de la classe Personne
+    /**
+     * @brief Identifiant de la personne
+     * @var int|null
+     */
     private ?int $idPersonne;
+
+    /**
+     * @brief Nom de la personne
+     * @var string|null
+     */
     private ?string $nom;
+
+    /**
+     * @brief Prénom de la personne
+     * @var string|null
+     */
     private ?string $prenom;
+
+    /**
+     * @brief Date de naissance de la personne
+     * @var string|null
+     */
     private ?string $dateNaiss;
+
+    /**
+     * @brief Genre de la personne
+     * @var string|null
+     */
     private ?string $genre;
 
-    // Constructeur de la classe Personne
+    /**
+     * @brief Constructeur de la classe Personne
+     * @param int|null $idPersonne Identifiant de la personne
+     * @param string|null $nom Nom de la personne
+     * @param string|null $prenom Prénom de la personne
+     * @param string|null $dateNaiss Date de naissance de la personne
+     * @param string|null $genre Genre de la personne
+     */
     public function __construct(?int $idPersonne = null, ?string $nom = null, ?string $prenom = null, ?string $dateNaiss = null, ?string $genre = null)
     {
         $this->idPersonne = $idPersonne;
@@ -19,52 +57,91 @@ class Personne
         $this->genre = $genre;
     }
 
-    // Getters et Setters
+    /**
+     * @brief Retourne l'identifiant de la personne
+     * @return int|null Identifiant de la personne
+     */
     public function getIdPersonne(): ?int
     {
         return $this->idPersonne;
     }
 
+    /**
+     * @brief Modifie l'identifiant de la personne
+     * @param int|null $idPersonne Identifiant de la personne
+     */
     public function setIdPersonne(?int $idPersonne): void
     {
         $this->idPersonne = $idPersonne;
     }
 
+    /**
+     * @brief Retourne le nom de la personne
+     * @return string|null Nom de la personne
+     */
     public function getNom(): ?string
     {
         return $this->nom;
     }
 
+    /**
+     * @brief Modifie le nom de la personne
+     * @param string|null $nom Nom de la personne
+     */
     public function setNom(?string $nom): void
     {
         $this->nom = $nom;
     }
 
+    /**
+     * @brief Retourne le prénom de la personne
+     * @return string|null Prénom de la personne
+     */
     public function getPrenom(): ?string
     {
         return $this->prenom;
     }
 
+    /**
+     * @brief Modifie le prénom de la personne
+     * @param string|null $prenom Prénom de la personne
+     */
     public function setPrenom(?string $prenom): void
     {
         $this->prenom = $prenom;
     }
 
+    /**
+     * @brief Retourne la date de naissance de la personne
+     * @return string|null Date de naissance de la personne
+     */
     public function getDateNaiss(): ?string
     {
         return $this->dateNaiss;
     }
 
+    /**
+     * @brief Modifie la date de naissance de la personne
+     * @param string|null $dateNaiss Date de naissance de la personne
+     */
     public function setDateNaiss(?string $dateNaiss): void
     {
         $this->dateNaiss = $dateNaiss;
     }
 
+    /**
+     * @brief Retourne le genre de la personne
+     * @return string|null Genre de la personne
+     */
     public function getGenre(): ?string
     {
         return $this->genre;
     }
 
+    /**
+     * @brief Modifie le genre de la personne
+     * @param string|null $genre Genre de la personne
+     */
     public function setGenre(?string $genre): void
     {
         $this->genre = $genre;
