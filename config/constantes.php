e1673954b3047564665d1f5611e6a1f310a69836
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-11-19 08:11:44 +0100

    Retour du fichier config parce qu'on peut pas le pull sinon

---
+++ config/constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+// Ce ne sont que des exemples
+//Connexion à la base de donnée
+define('DB_HOST', 'lakartxela.iutbayonne.univ-pau.fr');
+define('DB_NAME', 'namrein_pro');
+define('DB_USER', 'namrein_pro');
+define('DB_PASS', 'namrein_pro');
+define('PREFIXE_TABLE', 'vhs_');
+
+// Constante de la partie vue
+define('WEBSITE_TITLE', 'VHS - Video Home Share');
+define('WEBSITE_VERSION', '1.0');
+define('WEBSITE_DESCRIPTION', 'Un site de partage de films');
+define('WEBSITE_LANGUAGE', 'fr');
