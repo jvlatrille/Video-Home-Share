7a26472d87980e2c42a5377a0e81993938ce90c6
Jules VINET LATRILLE <jvlatrille@iutbayonne.univ-pau.fr>
2025-01-24 16:57:15 +0100

    A chaque connexion, la Bd est sauvegardée dans le fichier

--- utilitaire/backupBD.php
+++ utilitaire/backupBD.php
@@ -1,46 +1,43 @@
 <?php
-// Inclure l'autoloader de Composer (si nécessaire)
-require_once __DIR__ . '/../vendor/autoload.php';
+/**
+ * @file backupBD.php 
+ * @brief Script permettant de sauvegarder la BD réguliérement grâce au planificateur de tâches windows
+ * @version 1
+ * @date 24/01/2025
+ * @author VINET LATRILLE Jules
+ */
 
+require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
 
-// Chemin vers le fichier constantes.yaml
+// On charge les constantes
 $configPath = __DIR__ . '/../config/constantes.yaml';
-
-// Charger les constantes YAML
 if (!file_exists($configPath)) {
     die("Erreur : Le fichier constantes.yaml est introuvable.\n");
 }
-
 $config = Yaml::parseFile($configPath);
 $dbConfig = $config['DB_'];
 $prefixeTable = $config['PREFIXE_TABLE'];
 
-// Connexion à la base de données
+// Connexion à la BD
 $host = $dbConfig['HOST'];
 $dbName = $dbConfig['NAME'];
 $user = $dbConfig['USER'];
 $password = $dbConfig['PASS'];
-
 $conn = new mysqli($host, $user, $password, $dbName);
 if ($conn->connect_error) {
     die("Erreur de connexion : " . $conn->connect_error);
 }
 
-// Configurer l'encodage UTF-8
+// Configurer l'encodage UTF-8 (pour les accents)
 $conn->set_charset("utf8mb4");
 
-// Générer le nom du fichier de sauvegarde
+// Nom du fichier de sauvegarde avec année-mois-etc...
 $date = date('Y-m-d-H-i-s');
 $backupDir = __DIR__ . '/../backupBD/';
 $backupFile = $backupDir . $date . '.sql';
 
-// Vérifier si le dossier backupBD existe, sinon le créer
-if (!is_dir($backupDir)) {
-    mkdir($backupDir, 0777, true);
-}
-
-// Préparer la sauvegarde
+// Joli mise en page :)
 $backupSql = "-- Sauvegarde complète des tables préfixées par $prefixeTable dans $dbName\n";
 $backupSql .= "-- Générée le $date\n\n";
 
@@ -82,6 +79,9 @@ while ($row = $tables->fetch_row()) {
 
 // Écrire le fichier de sauvegarde
 file_put_contents($backupFile, $backupSql);
+
+// Insérer la date de sauvegarde dans la table `vhs_derniereSave`
+$conn->query("INSERT INTO vhs_derniereSave (date_save) VALUES ('$date')");
 
 // Terminer le script
 echo "Sauvegarde complète des tables avec préfixe \"$prefixeTable\" terminée dans : $backupFile\n";
