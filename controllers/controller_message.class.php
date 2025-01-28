5b6a9f1742b31ae5a6dbee1bdea5a9dd412ba66c
Aymeric Pigeon <aymericpigeon798@gmail.com>
2025-01-28 14:11:47 +0100

    Suppression de chargerPhotoProfil qui ne sert a rien finalement

--- controllers/controller_message.class.php
+++ controllers/controller_message.class.php
@@ -62,30 +62,6 @@ class ControllerMessage extends Controller
     }
 }
 
-    public function chargerPhotoProfil()
-    {
-        // Vérifie si un utilisateur est connecté
-        if (isset($_SESSION['utilisateur'])) {
-            $utilisateurConnecte = unserialize($_SESSION['utilisateur']);
-            //Recupere l'id de l'utilisateur'
-            $idUtilisateur = $utilisateurConnecte->getIdUtilisateur();
-
-
-            // Récupère les messages postés par l'utilisateur
-            $managerMessage = new MessageDao($this->getPdo());
-            $messageListe = $managerMessage->chargerPhotoProfilMessage($idUtilisateur);
-
-            // Génère la vue 
-            $template = $this->getTwig()->load('forum_detail.html.twig');
-            echo $template->render(['messageListe' => $messageListe, 'utilisateur' => $utilisateurConnecte]);
-
-        }
-        else {
-            // Sinon, affiche la page de connexion
-            $template = $this->getTwig()->load('connexion.html.twig');
-            echo $template->render();
-        }
-    }
 
     public function like()
     {
