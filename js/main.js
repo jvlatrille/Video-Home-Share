578670457f2d4d1d487c438379876af1f2403c2c
Thibault CHIPY <chipythibault@icloud.com>
2025-02-27 17:05:26 +0100

    amélioration globale sur : le js, rechercher une série, une watchlist de la commu, la connexion

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import 'js/formConnexion.js';
+import 'js/formInscription.js';
+import 'js/noterFilm.js';
+import 'js/patientez.js';
+import 'js/searchSuggestions.js';
+import 'js/formCreaWatchlist.js';
+import 'js/rechercheWatchlistCommu.js';
+
